2df52001f9d115ebf44cb7bf23c9c0e2edd97517
TAMILRTS <52026198+TAMILRTS@users.noreply.github.com>
2019-06-20 12:21:47 +0530

    Create maximl.py

---
+++ maximl.py
@@ -0,0 +1,8 @@
+n=[]
+s=str(input())                                                                          1111
+for i in range(0,len(s)):
+  n.append(s[i])
+a=set(n)
+b=list(a)
+c=len(b)
+print(c)
